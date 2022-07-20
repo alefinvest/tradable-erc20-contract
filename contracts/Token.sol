444b4a3321456c6a0fdb72aaf055d41dee867667
Lucas Vieira <lucasvieira94@poli.ufrj.br>
2022-07-20 17:36:22 -0300

    Finished trading feature and tests inside erc20 token contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,20 +4,29 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 import "./Stakable.sol";
+import "./Tradable.sol";
 
-contract Token is Ownable, Stakable {
+contract Token is Ownable, Stakable, Tradable {
     uint256 private _totalSupply;
+    uint256 private _currentTokenPrice;
     uint8 private _decimals;
     string private _symbol;
     string private _name;
 
     mapping (address => uint256) private _balances;
-
     mapping (address => mapping (address => uint256)) private _allowances;
+    mapping (address => uint256) private _etherBalances;
+    mapping (address => bool) private _locked;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
-
     event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    modifier noReentrant() {
+        require(!_locked[msg.sender], "Token: no reentrancy");
+        _locked[msg.sender] = true;
+        _;
+        _locked[msg.sender] = false;
+    }
 
     constructor(uint256 tokenTotalSupply, string memory tokenName, string memory tokenSymbol, uint8 tokenDecimals) {
         _totalSupply = tokenTotalSupply;
@@ -26,6 +35,9 @@ contract Token is Ownable, Stakable {
         _decimals = tokenDecimals;
 
         _balances[msg.sender] = _totalSupply;
+        
+        _currentTokenPrice = 10 ** 16;
+        _placeOrder(_totalSupply, _currentTokenPrice);
 
         emit Transfer(address(0), msg.sender, _totalSupply);
     }
@@ -128,5 +140,59 @@ contract Token is Ownable, Stakable {
     function unstake(uint256 amount) public {
         uint256 stakes = _unstake(amount);
         _mint(msg.sender, stakes);
+    }
+
+    function placeOrder(uint256 amount, uint256 price) public {
+        require(amount <= _balances[msg.sender], "Token: cannot place an order with more than you own");
+        _burn(msg.sender, amount);
+        _placeOrder(amount, price);
+    }
+
+    function removeOrder(uint256 orderId) public {
+        _removeOrder(orderId);
+        SellOrder memory order = _getOrder(orderId);
+        _mint(msg.sender, order.amount);
+    }
+
+    function buyOrder(uint256 orderId, uint256 amount) public payable noReentrant {
+        SellOrder memory order = _getOrder((orderId));
+        uint256 orderPrice = order.price * amount;
+        require(msg.value >= orderPrice, "Token: ether sent must be greater or equal orderPrice.");        
+        address owner = owner();
+        uint256 ownerFee = msg.value / 100;
+        uint256 sellerPayment = msg.value - ownerFee;
+        _etherBalances[order.seller] += sellerPayment;
+        _etherBalances[owner] += ownerFee;
+        _buyOrder(orderId, amount);
+        _currentTokenPrice = order.price;
+        _mint(msg.sender, amount);
+    }
+
+    function withdraw() public payable noReentrant {
+        require(_etherBalances[msg.sender] > 0, "Token: no ether left to withdraw.");
+        uint256 amount = _etherBalances[msg.sender];
+        _etherBalances[msg.sender] = 0;
+        (bool success, ) = (msg.sender).call{ value: amount }("");
+        require(success, "Token: withdraw failed.");
+    }
+
+    function updateOrderPrice(uint256 orderId, uint256 newPrice) public {
+        _updateOrderPrice(orderId, newPrice);
+    }
+
+    function getEtherBalance() public view returns (uint256) {
+        return _etherBalances[msg.sender];
+    }
+
+    function getOrder(uint256 orderId) public view returns (SellOrder memory) {
+        return _getOrder(orderId);
+    }
+
+    function listOrders() public view returns (SellOrder[] memory) {
+        return _listOrders();
+    }
+
+    function getCurrentTokenPrice() public view returns (uint256) {
+        return _currentTokenPrice;
     }
  }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 require("@nomiclabs/hardhat-waffle");
 require("hardhat-gas-reporter");
 require("./scripts/deploy.js");
@@ -6,13 +7,13 @@ module.exports = {
   solidity: "0.8.4",
   networks: {
     rinkeby: {
-      url: RINKEBY_URL,
-      accounts: [DEV_PRIVATE_KEY],
+      url: process.env.RINKEBY_URL,
+      accounts: [process.env.DEV_PRIVATE_KEY],
     },
   },
   gasReporter: {
     enabled: true,
     currency: "BRL",
-    coinmarketcap: COINMARKETCAP_API_KEY,
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
   },
 };
